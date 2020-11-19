e3bfed702742e5741a76f4dacd9d3238be3c737c
Russell Owen <rowen@uw.edu>
2020-11-18 20:31:59 -0800

    Update the commander to handle tel_rotation better
    Ignore the timestamp when comparing to the previous sample
    to avoid printing every sample.
    Update the tel_motors callback to work in a similar way.

--- python/lsst/ts/mtrotator/rotator_commander.py
+++ python/lsst/ts/mtrotator/rotator_commander.py
@@ -22,8 +22,6 @@ __all__ = ["RotatorCommander"]
 import asyncio
 import math
 
-import numpy as np
-
 from lsst.ts import salobj
 
 STD_TIMEOUT = 5  # timeout for command ack
@@ -64,14 +62,73 @@ class RotatorCommander(salobj.CscCommander):
         kwargs = self.check_arguments(args, "start_position", "amplitude", "period")
         self.tracking_task = asyncio.ensure_future(self._sine(**kwargs))
 
-    async def tel_motors_callback(self, data):
-        rounded_value = np.around(data.calibrated, decimals=3)
-        if np.array_equal(self.previous_tel_motors, rounded_value):
+    def format_dict(self, **kwargs):
+        """Return telemetry or event data formatted for printing.
+
+        This variant of format_data works is provided to support
+        `_special_telemetry_callback`.
+
+        The formatting the same as `format_data`: a comma-separate list
+        of items formatted by `format_item`.
+        """
+        return ", ".join(self.format_item(key, value) for key, value in kwargs.items())
+
+    def _special_telemetry_callback(self, data, name, omit_field):
+        """Callback for telemetry omitting one specified field
+        from the comparison, but printing it.
+
+        Parameters
+        ----------
+        data : `object`
+            Telemetry data.
+        name : `str`
+            Name of telemetry topic, without the `tel_` prefix.
+        omit_field : `list` [`str`]
+            Field to omit from the comparison.
+        """
+        prev_value_name = f"previous_tel_{name}"
+        public_fields = self.get_rounded_public_fields(data)
+        omit_value = public_fields.pop(omit_field)
+        if public_fields == getattr(self, prev_value_name):
             return
-        self.previous_tel_motors = rounded_value
-        print(f"motors: {self.format_data(data)}")
+        setattr(self, prev_value_name, public_fields)
+        formatted_data = self.format_dict(**public_fields, **{omit_field: omit_value})
+        self.output(f"{data.private_sndStamp:0.3f}: {name}: {formatted_data}")
+
+    async def tel_motors_callback(self, data):
+        """Don't print if only the raw field has changed.
+
+        Parameters
+        ----------
+        data : `object`
+            MTRotator motors telemetry data.
+        """
+        self._special_telemetry_callback(data=data, name="motors", omit_field="raw")
+
+    async def tel_rotation_callback(self, data):
+        """Don't print if only the timestamp has changed.
+
+        Parameters
+        ----------
+        data : `object`
+            MTRotator rotation telemetry data.
+        """
+        self._special_telemetry_callback(
+            data=data, name="rotation", omit_field="timestamp"
+        )
 
     async def _ramp(self, start_position, end_position, velocity):
+        """Track a linear ramp.
+
+        Parameters
+        ----------
+        start_position : `float`
+            Starting position of ramp (deg).
+        end_position : `float`
+            Ending position of ramp (deg).
+        velocity : `float`
+            Velocity of motion along the ramp (deg/sec).
+        """
         try:
             if velocity == 0:
                 raise ValueError(f"velocity {velocity} must be nonzero")
@@ -102,6 +159,20 @@ class RotatorCommander(salobj.CscCommander):
             await self.remote.cmd_stop.start(timeout=STD_TIMEOUT)
 
     async def _sine(self, start_position, amplitude, period):
+        """Track one sine wave of specified amplitude and period.
+
+        The range of motion is period - amplitude to period + amplitude,
+        plus whatever motion is required to slew to the path.
+
+        Parameters
+        ----------
+        start_position : `float`
+            Midpoint of sine wave (deg).
+        amplitude : `float`
+            Amplitude of sine wave (deg).
+        period : `float`
+            Duration of motion: one full wave (sec).
+        """
         try:
             if period <= 0:
                 raise ValueError(f"period {period} must be positive")
